0a5a7c59437fadefbb9f473829c4f519837750e0
Irving <zhouyongtao@outlook.com>
2014-04-12 01:54:14 +0800

    第一次提交

---
+++ PluploadMVC4Demo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PluploadMVC4Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            for (int i = 0; i < Request.Files.Count; i++)
+            {
+                var file = Request.Files[i];
+                file.SaveAs(AppDomain.CurrentDomain.BaseDirectory + "Uploads/" + file.FileName);
+            }
+            return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
